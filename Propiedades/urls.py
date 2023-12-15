aeb124cc768147b40f2e23deaaf5331d7ce04072
José Ibañez <jeduardo_ibanez@ciencias.unam.mx>
2023-12-15 14:48:57 -0600

    Actualización de los endpoints
    Se hizo una actualización para que los endpoints se ejecutaran mediante url

--- Propiedades/urls.py
+++ Propiedades/urls.py
@@ -1,7 +1,17 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import PropertyViewSet
 
 router = DefaultRouter()
 router.register(r'api/properties', PropertyViewSet, basename='property')
 
-urlpatterns = router.urls
+#urlpatterns = [
+urlpatterns = router.urls + [
+    path('api/properties/listing/', PropertyViewSet.as_view({'get' : 'list_all_properties'}), name='list_all_properties'),
+    path('api/properties/<int:pk>/retrieve_property/', PropertyViewSet.as_view({'get': 'retrieve_single_property'}), name='retrieve_single_property'),
+    path('api/properties/create/', PropertyViewSet.as_view({'post': 'create_property'}), name='create_property'),
+    path('api/properties/<int:pk>/update/', PropertyViewSet.as_view({'put': 'update_property'}), name='update_property'),
+    path('api/properties/<int:pk>/delete/', PropertyViewSet.as_view({'delete': 'delete_property'}), name='delete_property'),
+]
+    #path('api/properties/<int:id_property>/delete_property/', DeletePropertyView.as_view(), name='delete_property'),
+
--- Propiedades/views.py
+++ Propiedades/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 #Importamos nuestro modelo.
 from .models import Property
 #Importamos nuestro serializer 
@@ -9,39 +13,44 @@ from .serializers import PropertySerializer
 #Creamos las vistas
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
-    #permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny]
     serializer_class = PropertySerializer
 
     #Definimos el endpoint para listar
-    def listing(self, request, *args, **kwargs):
+    @action(detail=False, methods=['get'], url_path='listing')
+    def list_all_properties(self, request):
         properties = Property.objects.all()
         serializer = self.get_serializer(properties, many=True)
         return Response(serializer.data)
     
     #Definimos el endpoint para devolver una propiedad
-    def retrieving(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
+    @action(detail=True, methods=['get'], url_path='retrieve_property')
+    def retrieve_single_property(self, request, pk=None):
+        property_instance = self.get_object()
+        serializer = self.get_serializer(property_instance)
         return Response(serializer.data)
     
     #Definimos el endpoint para crear una propiedad
-    def creating(self, request, *args, **kwargs):
+    @action(detail=False, methods=['post'], url_path='create')
+    def create_property(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
 
     #Definimos el endpoint para actualizar 
-    def updating(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    @action(detail=True, methods=['put'], url_path='update')
+    def update_property(self, request, pk=None):
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     #Definimos el endpoint para eliminar
-    def destroying(self, request, *args, **kwargs):
+    @action(detail=True, methods=['delete'], url_path='delete')
+    def delete_property(self, request, pk=None):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
